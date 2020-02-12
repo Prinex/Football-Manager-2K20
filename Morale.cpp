aa344530f1228979e91a66107159c45ff9bc6c92
SaddestCat <36926082+SaddestCat@users.noreply.github.com>
2020-02-12 17:59:40 +0000

    Add files via upload

---
+++ Morale.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class FanNoTeamPlay{
+    protected:
+        int fanMorale;
+        int capacity;
+        int weatherMult;
+        int adMult;
+        int leagueID;
+        int attempts;
+        int goals;
+
+        int teamPlay;
+        int teamRating;
+        int score;
+
+    public:
+        void updateFanMorale(){
+            fanMorale = ((capacity)/1000) * (1+adMult) * (weatherMult) + 100*(leagueID+1) + 10*(score+attempts)
+        }
+        int getFanMorale(){
+            return fanMorale
+        }
+
+        void updateTeamPlay(){
+            teamPlay (goals - lossses)*100 + teamRating
+        }
+
+        int getTeamPlay(){
+            return teamPlay
+        }
+
+}
+
+class Morale: public FanTeamPlay{
+    protected:
+        int morale;
+        
+    public:
+        void updateMorale(){
+            updateFanMorale();
+            updateTeamPlay();
+            morale = (getTeamPlay + getFanMorale) / 2;
+
+        }
+        int getMorale(){
+            return morale;
+        }
+}
