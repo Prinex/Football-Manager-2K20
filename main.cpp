261eceb3e1a7d91896c35540a1d017689dc880a4
Daniel Calota <calotad@coventry.ac.uk>
2020-02-19 12:43:14 +0000

    Update main func

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <windows.h>
+
 #include "gameFunc.h"
 
-using namespace std;
+
+
+
 
 int main()
 {
-    MainWindow options;
 
-    options.MainMenu();
+    GameWindow init;
+    init.InitWindow();
+
+
+
+
 
 
     return 0;
