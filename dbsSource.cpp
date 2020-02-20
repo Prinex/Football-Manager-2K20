4f498b449b6674f921c40072ec187fd6723d1848
SaddestCat <36926082+SaddestCat@users.noreply.github.com>
2020-02-20 14:23:31 +0000

    Update dbsSource.cpp

--- dbsSource.cpp
+++ dbsSource.cpp
@@ -2,14 +2,11 @@
 #include <iostream>
 #include <iomanip>
 
-
-<<<<<<< HEAD
 Players::Players(const std::string &name, int age, const std::string &nationality, int height, double weight, int number, const std::string &position,
                  const std::string &morale, int value, int pace, int shooting, int passing, int dribbling, int defending, int physical, int reflexes,
-=======
+
 Players::Players(const std::string &name, int age, const std::string &nationality, int height, double weight, int number, std::string position,
                  std::string morale, int value, int pace, int shooting, int passing, int dribbling, int defending, int physical, int reflexes,
->>>>>>> 61621e567c5dbc5ffbac9bb4008c6db8632d0af7
                  int diving, int handling, int skill_position, int overall_points) :
                      name(name),
                      age(age),
@@ -57,11 +54,11 @@ void Players::PrintPlayers(std::vector<Players> &players)
 
 
 
-Squad::Squad(const std::string &squad_name, const std::string &league, const std::string &manager, const int noInField, const int noSub) :
+Squad::Squad(const std::string &squad_name, const std::string &league, const std::string &manager, const int noInField, const int noSub)
+{
     squad_name{squad_name},
     league{league},
     manager(manager),
     noInField{noInField},
     noSub{noSub}
-{
 }
