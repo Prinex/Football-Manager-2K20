3e66eef90e673accd1d26778c70ae05a8f0075fd
Prinex <songdahye00@gmail.com>
2021-01-06 21:39:47 +0000

    A script file for building the project

---
+++ buildPrj.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Choose a build option: "
+echo "1. Build the project using VS 16 2019 x64"
+echo "2. Build the project using MinGW Makfiles"
+
+read OPTION
+
+echo "Building the project..."
+rm -rf build
+rm -rf bin
+rm -rf lib
+
+mkdir build
+cd build
+
+if [ $OPTION == 1 ]; then
+	cmake .. -G "Visual Studio 16 2019"
+elif [ $OPTION == 2 ]; then
+	cmake .. -G "MinGW Makefiles"
+	
+else
+	echo "Invalid option"
+fi
